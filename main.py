846f1410659172ffcc620161d1c2406889598a73
Nat Hurtig <keep@nathanhurtig.com>
2024-10-13 07:58:20 -0700

    Linters!

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+"""
+This is just a placeholder module; Nat will
+expand this later to be an actual app
+"""
+
 def main() -> None:
+    """
+    Simple hello, world
+    """
     print("Hello, world!")
 
 if __name__ == "__main__":
