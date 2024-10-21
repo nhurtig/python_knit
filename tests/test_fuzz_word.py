182f3f19ba32de3964b5f415fe6c74f0f2ec8460
Nat Hurtig <nhurtig@acm.org>
2024-10-21 16:52:54 -0700

    Make the fuzzing possibly infinite, write updates to a file, and fuzz larger words

--- tests/test_fuzz_word.py
+++ tests/test_fuzz_word.py
@@ -11,21 +11,24 @@ from layer.layer import Layer
 from layer.word import CanonWord, Word
 from tests.test_fuzz_braid import random_braid_word
 
-MIN_BOXES = 0
-MAX_BOXES = 3
+MIN_BOXES = 1
+MAX_BOXES = 5
 MIN_INS = 0
 MAX_INS = 5
 MIN_OUTS = 2
 MAX_OUTS = 5
 WORDS_PER_NUM_BOXES = 20
-LETTERS_PER_WORD = 6
+LETTERS_PER_WORD = 10
 MUTANTS_PER_WORD = 20
-LAYER_MUTATIONS_PER_LAYER = 2
-BRAID_MUTATIONS_PER_BRAID = 10
+LAYER_MUTATIONS_PER_LAYER = 3
+BRAID_MUTATIONS_PER_BRAID = 15
+FUZZ_FOREVER = False
+TEST_OUT_INFO = "word_fuzz_out.txt"
+UPDATE_FREQ = 100
 # Took around 50 seconds last time
 
 rng = random.random
-random.seed(30)
+random.seed(28)
 
 
 def random_word(num_boxes: int) -> Word:
@@ -59,23 +62,30 @@ def test_word_canonicalization_fuzzing() -> None:
     """Tests the word canonicalization by fuzzing various
     words, computing their canonical forms, and asserting
     the canonical fuzzed is the same as the canonical original."""
-    for num_boxes in range(MIN_BOXES, MAX_BOXES + 1):
-        for _ in range(WORDS_PER_NUM_BOXES):
-            # Generate a random word
-            original = random_word(num_boxes)
-            original_canon = CanonWord(original.copy())
+    tests = 0
+    while FUZZ_FOREVER:
+        for num_boxes in range(MIN_BOXES, MAX_BOXES + 1):
+            for _ in range(WORDS_PER_NUM_BOXES):
+                # Generate a random word
+                original = random_word(num_boxes)
+                original_canon = CanonWord(original.copy())
 
-            for _ in range(MUTANTS_PER_WORD):
-                # Create a mutant of the original braid by fuzzing it
-                mutant = original.copy()
-                mutant.fuzz(
-                    random.random,
-                    LAYER_MUTATIONS_PER_LAYER,
-                    BRAID_MUTATIONS_PER_BRAID,
-                )
+                for _ in range(MUTANTS_PER_WORD):
+                    # Create a mutant of the original braid by fuzzing it
+                    mutant = original.copy()
+                    mutant.fuzz(
+                        random.random,
+                        LAYER_MUTATIONS_PER_LAYER,
+                        BRAID_MUTATIONS_PER_BRAID,
+                    )
 
-                # Check that the canonical forms of the original and mutant are equivalent
-                mutant_canon = CanonWord(mutant)
-                assert (
-                    original_canon == mutant_canon
-                ), f"Word mismatch after fuzzing for num_boxes={num_boxes}"
+                    # Check that the canonical forms of the original and mutant are equivalent
+                    mutant_canon = CanonWord(mutant)
+                    assert (
+                        original_canon == mutant_canon
+                    ), f"Word mismatch after fuzzing for num_boxes={num_boxes}"
+                    tests += 1
+
+                    if tests % UPDATE_FREQ == 0:
+                        with open(TEST_OUT_INFO, "w+", encoding='utf-8') as f:
+                            f.write(f"tested {tests} words so far")
